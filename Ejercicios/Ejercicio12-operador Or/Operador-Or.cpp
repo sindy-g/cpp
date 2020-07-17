f02bb34e6038831aaaf04c11b83c735e9ac5c9ca
Sindy Galdamez <sgaldamez18@gmail.com>
2020-07-16 23:59:22 -0600

    Agregando ejercicio Or

---
+++ Ejercicios/Ejercicio12-operador Or/Operador-Or.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 10;
+    int b = 12;
+    int c = 7;
+
+    if (a > 8 && b > 8 && c > 8){
+        cout<< "Todos los numeros son mayor que 8";
+    }
+     if (a > 8 || b > 8 || c > 8){
+        cout<< "Algun numero es mayor que 8";
+    }
+    
+    return 0;
+}
