9fbcdeaee95f3804e031c44ad7f93ee99dec7365
Sindy Galdamez <sgaldamez18@gmail.com>
2020-08-03 17:09:52 -0600

    Agregando repaso

---
+++ Ejercicios/Ejercicio26-Repaso1/repaso.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    for(int=1; i<=10; i++)
+    {
+        cout<<"Indice: "<<i;
+        cout<< endl;
+    }
+    cout<< endl;
+    cout<< endl;
+    int i=1;
+    while (i<=10)
+    {
+        cout<<"Indice: "<<i;
+        cout<< endl;
+        i++;
+    }
+    
+
+    return 0;
+}
