66febc439af3c3dff464a4bc3116949e1a214d2d
Sindy Galdamez <sgaldamez18@gmail.com>
2020-08-03 19:07:35 -0600

    Agregando funcion parametros

---
+++ Ejercicios/Ejercicio30-Funcion int con parametros/parametros.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int sumar(int a, int b){
+    return a+b;
+}
+int restar(int a, int b){
+    return a-b;
+}
+int main(int argc, char const *argv[])
+{
+    system("cls");
+
+    int numero1;
+    int numero2;
+
+    cout<<"Ingrese el valor de a: ";
+    cin>> numero1;
+    cout<<"Ingrese el valor de b: ";
+    cin>> numero2;
+
+    cout<<endl;
+    cout<<"El resultado de la suma es: "<<sumar(numero1,numero2);
+    cout<<endl;
+     cout<<"El resultado de la resta es: "<<restar(numero1,numero2);
+    cout<<endl;
+
+    sumar(8,9);
+
+
+    return 0;
+}
+
