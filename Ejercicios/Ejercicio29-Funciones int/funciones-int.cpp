595726d0e27083f29d0aec51c7908712be9a4c53
Sindy Galdamez <sgaldamez18@gmail.com>
2020-08-03 18:42:38 -0600

    Agregando funciones int

---
+++ Ejercicios/Ejercicio29-Funciones int/funciones-int.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int sumar(int a, int b){
+    return a + b;
+}
+int main(int argc, char const *argv[])
+{
+    system("cls");
+
+    int resultado = sumar (8,3);
+    cout<<sumar(5,7);
+    cout<<endl;
+    cout<<sumar(15,7);
+    cout<<endl;
+    cout<<sumar(5,71);
+    cout<<endl;
+    cout<<sumar(9,8);
+    cout<<endl;
+    return 0;
+}
