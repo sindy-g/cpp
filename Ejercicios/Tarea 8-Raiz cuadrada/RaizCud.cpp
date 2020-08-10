c5d7ffc53ad63fec1da892844b57443449a232bd
Sindy Galdamez <sgaldamez18@gmail.com>
2020-08-10 17:44:38 -0600

    Agregando tarea 8

---
+++ Ejercicios/Tarea 8-Raiz cuadrada/RaizCud.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double num=0;
+    double raizCuadrada=0;
+
+    cout<<"Ingrese un numero: "<<num<<endl;
+
+    raizCuadrada=sqrt(num);
+    cout<<endl;
+    cout<<"La raiz cuadrada es: "<<raizCuadrada<<endl;
+    return 0;
+}
+
