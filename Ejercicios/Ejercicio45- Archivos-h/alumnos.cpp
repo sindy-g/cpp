f5a3ad1eb453c0ddf5a4501f56c8dc7fa3514b37
Sindy Galdamez <sgaldamez18@gmail.com>
2020-08-13 23:45:04 -0600

    Agregando ejercicio 45

---
+++ Ejercicios/Ejercicio45- Archivos-h/alumnos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+string nombre="Juan";
+string apellido="Perez";
+int edad=18;
+
+string obtenerNombreCompleto()
+{
+    return nombre+" "+ apellido;
+}
+int obtenerEdad()
+{
+    return edad;
+}
