4b657244a53350f1bc206fb47f0d879ae296bd95
Sindy Galdamez <sgaldamez18@gmail.com>
2020-08-13 21:22:58 -0600

    Agregando ejercicio 39

---
+++ Ejercicios/Ejercicio39-Modulacion/modulacion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+extern int sumar(int a, int b);
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    int resultado=sumar(8,3);
+
+    cout<< sumar (5,7);
+    cout<<endl;
+    return 0;
+}
