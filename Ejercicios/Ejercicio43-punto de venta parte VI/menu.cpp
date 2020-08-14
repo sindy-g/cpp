3b04622d9ace94a9de26f4f7b315e6d1573e81b4
Sindy Galdamez <sgaldamez18@gmail.com>
2020-08-13 23:08:16 -0600

    Agregando ejercicio 43

---
+++ Ejercicios/Ejercicio43-punto de venta parte VI/menu.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+extern void productos(int opcion);
+extern void imprimirFactura();
+void menu()
+{
+    int opcion=0;
+    while (true)
+    {
+        system("cls");
+        cout<<"*****";
+        cout<<"MENU";
+        cout<<"*****";
+
+        cout<<endl;
+        cout<<"1-Bebidas calientes"<<endl;
+        cout<<"2-Bebidas frias"<<endl;
+        cout<<"3-Reposteria"<<endl;
+        cout<<"4-Imrpimir Factura"<<endl;
+        cout<<"0-Salir"<<endl;
+        cin>> opcion;
+
+        if(opcion==0)
+        {
+            break;
+        }
+        if(opcion==4)
+        {
+            imprimirFactura();
+        } else{
+            productos(opcion);
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio43-punto de venta parte VI/productos.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+extern void agregarProducto(string descripcion, int cantidad, double precio);
+
+void productos(int opcion)
+{
+    system("cls");
+    int opcionProducto=0;
+    switch (opcion)
+    {
+    case 1: 
+    {
+        cout<<"BEBIDAS CALIENTES"<<endl;
+        cout<<"******************";
+        cout<<"1-Capuccino"<<endl;
+        cout<<"2-Espresso"<<endl;
+        cin>> opcionProducto;
+
+        switch (opcionProducto)
+        {
+        case 1: agregarProducto("Capuccino",1,40);
+            break;
+            case 2: agregarProducto("Espresso",1,30);
+            
+        default: cout<<"Opcion no valida";
+        return;
+            break;
+        }
+    
+        
+    }
+     case 2: 
+    {
+        cout<<"BEBIDAS FRIAS"<<endl;
+        cout<<"**************";
+        system("pause");
+    
+        break;
+    }
+     case 3: 
+    {
+        cout<<"REPOSTERIA"<<endl;
+        cout<<"***********";
+        system("pause");
+    
+        break;
+    }
+    
+    }
