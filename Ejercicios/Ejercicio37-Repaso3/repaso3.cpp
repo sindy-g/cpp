0abee92ebc4d00e80f976beabda8553f8567df75
Sindy Galdamez <sgaldamez18@gmail.com>
2020-08-13 18:53:04 -0600

    Agregando ejercicio 37

---
+++ Ejercicios/Ejercicio37-Repaso3/repaso3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a=0;
+    int b=0;
+    int c=0;
+
+    //if(a>b && a>c){
+        //cout<<"a es mayor que b o mayor que c";
+    // }
+    //while(a>b){
+     //a--;
+    //}
+    //if(a=20){
+        //break;
+    //}
+    //}
+    while(true)
+    {
+        while(true){
+            if (a==10)
+            {
+                break;
+            } b++;
+        } if(a==1){
+            break;
+        }a++;
+    }
+    return 0;
+}
